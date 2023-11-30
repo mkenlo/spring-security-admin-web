506336b2e38c9429b2973956057581bd81f75d8a
Melanie <mel.lontsi@gmail.com>
2023-11-29 21:44:05 -0800

    basic authentication is wired with jdbc

--- src/main/java/com/mkenlo/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/mkenlo/adminweb/config/WebSecurityConfig.java
@@ -7,8 +7,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class WebSecurityConfig {
@@ -20,12 +25,12 @@ public class WebSecurityConfig {
         return http.build();
     }
     @Bean
-    public UserDetailsService users(){
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsService users(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public static PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
