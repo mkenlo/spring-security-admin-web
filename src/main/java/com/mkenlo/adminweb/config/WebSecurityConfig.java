76dcd385bbe2d0a0e256f63758ad7dcd54f12600
Melanie <mel.lontsi@gmail.com>
2023-11-30 16:21:01 -0800

    implemented form-based authentication with a login/logout flow

--- src/main/java/com/mkenlo/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/mkenlo/adminweb/config/WebSecurityConfig.java
@@ -2,16 +2,10 @@ package com.mkenlo.adminweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -28,8 +22,15 @@ public class WebSecurityConfig {
                         .requestMatchers("/customers/**").hasRole("USER")
                         .requestMatchers("/orders").hasRole("ADMIN")
                         .anyRequest().authenticated())
-
-                .httpBasic(Customizer.withDefaults());
+                .formLogin(form -> form.loginPage("/login")
+                                .failureUrl("/login?error")
+                                .permitAll()
+                        )
+                .logout(form -> form.clearAuthentication(true)
+                                .invalidateHttpSession(true)
+                                .logoutSuccessUrl("/login?logout")
+                                .permitAll()
+                        );
         return http.build();
     }
     @Bean
